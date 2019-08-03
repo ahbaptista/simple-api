d3badc92929b1ae6079e9646f5bb2c8f93e496ce
Aderito Baptista <a.baptista@elsevier.com>
2019-08-03 22:33:38 +0100

    comemnts

--- src/main/java/com/example/simpleapi/service/ItemService.java
+++ src/main/java/com/example/simpleapi/service/ItemService.java
@@ -28,5 +28,6 @@ public class ItemService {
                         .setPrice(item.getPrice())
                         .build())
                 .collect(Collectors.toList());
+        //few comments to trigger the pipe
     }
 }
