715dbcde3d3713fe298cc496ac20fae50841566f
Aderito Baptista <a.baptista@elsevier.com>
2019-08-03 23:08:53 +0100

    comment to trigger the pipe

--- src/main/java/com/example/simpleapi/service/ItemService.java
+++ src/main/java/com/example/simpleapi/service/ItemService.java
@@ -28,5 +28,6 @@ public class ItemService {
 //                        .setPrice(item.getPrice())
                         .build())
                 .collect(Collectors.toList());
+        //comment to trigger the pipe
     }
 }
